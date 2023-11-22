8388cabe5d70bd60dbf0c17918fa77ef1a4db336
HasanCoder66 <hasanashraf874@gmail.com>
2023-11-22 20:05:13 +0500

    26 done

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import './Nav.css'
+import React from "react";
+import "./Nav.css";
 function Nav() {
   return (
-    <div className='Nav'>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfqwQEHzAE83CL0k3orejpL55pIRkiCO1LHRB4zi9n&s' alt='Netflix Logo'/>
+    <div className="nav nav__black">
+      <div className="nav__Contents">
+        <img
+          src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=700&h=456"
+          alt="Netflix Logo"
+          className="nav__logo"
+        />
+
+        <img
+          src="https://thumbs.dreamstime.com/b/icon-profile-color-green-circle-dark-background-white-not-shadow-194702093.jpg"
+          className="nav__avatar"
+          alt="avatar"
+          // width={"100px"}
+        />
+      </div>
+      <h1>testy</h1>
+      <h1>testy</h1>
+      <h1>testy</h1>
+      <h1>testy</h1>
+      
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
