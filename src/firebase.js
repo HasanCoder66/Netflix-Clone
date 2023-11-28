99f1715c3cbdb95e269ffb076a19c85ead0ef7e2
HasanCoder66 <hasanashraf874@gmail.com>
2023-11-28 16:53:08 +0500

    FIREBASE START AND ENV FILE WORK DONE

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,12 @@
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCVp5KQSrxXarh9jjtBBYF8cXqILmjmwUI",
-    authDomain: "netflix-clone-cd5c9.firebaseapp.com",
-    projectId: "netflix-clone-cd5c9",
-    storageBucket: "netflix-clone-cd5c9.appspot.com",
-    messagingSenderId: "114186583695",
-    appId: "1:114186583695:web:254ac91eb482a394757afa"
-  };
+  apiKey: "AIzaSyCVp5KQSrxXarh9jjtBBYF8cXqILmjmwUI",
+  authDomain: "netflix-clone-cd5c9.firebaseapp.com",
+  projectId: "netflix-clone-cd5c9",
+  storageBucket: "netflix-clone-cd5c9.appspot.com",
+  messagingSenderId: "114186583695",
+  appId: "1:114186583695:web:254ac91eb482a394757afa"
+};
+
+const app = initializeApp(firebaseConfig);
